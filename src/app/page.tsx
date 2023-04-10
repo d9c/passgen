ee88c7fae9fcec15699b875369dd5a768577ebb9
d9c <davidgdoprado@gmail.com>
2023-04-10 18:32:23 -0300

    Add GitHub repo link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import IconButton from '@mui/material/IconButton';
 
 import ContentCopy from '@mui/icons-material/ContentCopy';
 import Cached from '@mui/icons-material/Cached';
+import GitHub from '@mui/icons-material/GitHub';
 
 import { Checkbox, Snackbar, Slider, Tooltip } from '@/components/MuiStyled';
 
@@ -98,75 +99,80 @@ export default function Home() {
   };
 
   return (
-    <div className="flex w-[640px] flex-col items-center gap-[20px]">
-      <div className="flex w-full items-center justify-between rounded-[4px] border-[1px] border-solid border-[#333] bg-[#111] px-[16px] py-[6px]">
-        <span className="text-[16px] text-[#FFF]" ref={pwdRef} />
-        <div className="flex items-center gap-[5px]">
-          <Tooltip title="Copy to Clipboard" placement="top">
-            <IconButton onClick={copyToClipboard}>
-              <ContentCopy color="primary" />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="New Password" placement="top">
-            <IconButton onClick={generatePassword}>
-              <Cached color="primary" />
-            </IconButton>
-          </Tooltip>
+    <div className="flex flex-col items-center gap-[50px]">
+      <div className="flex w-[640px] flex-col items-center gap-[20px]">
+        <div className="flex w-full items-center justify-between rounded-[4px] border-[1px] border-solid border-[#333] bg-[#111] px-[16px] py-[6px]">
+          <span className="text-[16px] text-[#FFF]" ref={pwdRef} />
+          <div className="flex items-center gap-[5px]">
+            <Tooltip title="Copy to Clipboard" placement="top">
+              <IconButton onClick={copyToClipboard}>
+                <ContentCopy color="primary" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="New Password" placement="top">
+              <IconButton onClick={generatePassword}>
+                <Cached color="primary" />
+              </IconButton>
+            </Tooltip>
+          </div>
         </div>
+        <div className="flex w-full flex-col gap-[10px]">
+          <div className="flex items-center justify-between">
+            <span className="text-[16px] text-[#FFF]">Length</span>
+            <Slider
+              min={1}
+              max={50}
+              size="small"
+              valueLabelDisplay="auto"
+              value={settings.length}
+              onChange={handleSliderChange}
+            />
+          </div>
+          <div className="content-bet flex items-center justify-between">
+            <span className="text-[16px] text-[#FFF]">Uppercase</span>
+            <Checkbox
+              name="uppercase"
+              checked={settings.uppercase}
+              onChange={handleCheckboxChange}
+            />
+          </div>
+          <div className="content-bet flex items-center justify-between">
+            <span className="text-[16px] text-[#FFF]">Lowercase</span>
+            <Checkbox
+              color="primary"
+              name="lowercase"
+              checked={settings.lowercase}
+              onChange={handleCheckboxChange}
+            />
+          </div>
+          <div className="content-bet flex items-center justify-between">
+            <span className="text-[16px] text-[#FFF]">Numbers</span>
+            <Checkbox
+              name="numbers"
+              checked={settings.numbers}
+              onChange={handleCheckboxChange}
+            />
+          </div>
+          <div className="content-bet flex items-center justify-between">
+            <span className="text-[16px] text-[#FFF]">Symbols</span>
+            <Checkbox
+              name="symbols"
+              checked={settings.symbols}
+              onChange={handleCheckboxChange}
+            />
+          </div>
+        </div>
+        <Snackbar
+          open={open}
+          onClose={handleClose}
+          autoHideDuration={2000}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+          message="Copied to clipboard"
+        />
       </div>
-      <div className="flex w-full flex-col gap-[10px]">
-        <div className="flex items-center justify-between">
-          <span className="text-[16px] text-[#FFF]">Length</span>
-          <Slider
-            min={1}
-            max={50}
-            size="small"
-            valueLabelDisplay="auto"
-            value={settings.length}
-            onChange={handleSliderChange}
-          />
-        </div>
-        <div className="content-bet flex items-center justify-between">
-          <span className="text-[16px] text-[#FFF]">Uppercase</span>
-          <Checkbox
-            name="uppercase"
-            checked={settings.uppercase}
-            onChange={handleCheckboxChange}
-          />
-        </div>
-        <div className="content-bet flex items-center justify-between">
-          <span className="text-[16px] text-[#FFF]">Lowercase</span>
-          <Checkbox
-            color="primary"
-            name="lowercase"
-            checked={settings.lowercase}
-            onChange={handleCheckboxChange}
-          />
-        </div>
-        <div className="content-bet flex items-center justify-between">
-          <span className="text-[16px] text-[#FFF]">Numbers</span>
-          <Checkbox
-            name="numbers"
-            checked={settings.numbers}
-            onChange={handleCheckboxChange}
-          />
-        </div>
-        <div className="content-bet flex items-center justify-between">
-          <span className="text-[16px] text-[#FFF]">Symbols</span>
-          <Checkbox
-            name="symbols"
-            checked={settings.symbols}
-            onChange={handleCheckboxChange}
-          />
-        </div>
-      </div>
-      <Snackbar
-        open={open}
-        onClose={handleClose}
-        autoHideDuration={2000}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        message="Copied to clipboard"
-      />
+      <a href="https://github.com/d9c/passgen" target="_blank">
+        <GitHub color="primary" />
+      </a>
     </div>
   );
 }
