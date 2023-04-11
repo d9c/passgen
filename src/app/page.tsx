5b9fc0fd0a8ab7209a736d5970ff0599f71361d8
d9c <davidgdoprado@gmail.com>
2023-04-11 19:25:16 -0300

    Reduce gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Generator } from '@/components/Generator';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-[50px]">
+    <div className="flex flex-col items-center gap-[30px]">
       <Generator />
       <a href="https://github.com/d9c/passgen" target="_blank">
         <GitHub color="primary" />
